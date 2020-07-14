e09870ab9a1aed10cf7e5503fed1f2333f8d8101
Lucas Lima <64433440+LucasLimaPinho@users.noreply.github.com>
2020-07-14 18:49:09 -0300

    Create functionsreturnfunctions.go

---
+++ functionsreturnfunctions.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func MakeDistOrigin(o_x,o_y float64) func(float64,float64) float64{
+	fn:= func (x,y float64) float64{
+		return math.Sqrt(math.Pow(x-o_x,2)+
+			math.Pow(y-o_y,2))
+	}
+	return fn
+}
