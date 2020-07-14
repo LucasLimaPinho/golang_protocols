f7fa978050d56ac465b61f962d66835865a63502
Lucas Lima <64433440+LucasLimaPinho@users.noreply.github.com>
2020-07-14 18:52:01 -0300

    Create variousargs.go

---
+++ variousargs.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func getMax(vals ... int) int {
+	maxV := -1
+	for _, v := range vals {
+		if v > maxV{
+			maxV = v
+		}
+	}
+	return maxV
+}
+
+func main(){
+	fmt.Println(getMax(1,2,3,4,5,6,7,8,9,10))
+
+}
