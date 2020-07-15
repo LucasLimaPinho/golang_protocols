0ab824290e47a9b7a954b32e08ca9f692cfec0e7
Lucas Lima <64433440+LucasLimaPinho@users.noreply.github.com>
2020-07-14 21:08:04 -0300

    Create methodwithPointerReference.go

---
+++ methodwithPointerReference.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct{
+	x float64
+	y float64
+}
+
+func (p *Point) DistToOrigin() float64 {
+	t := math.Pow(p.x,2) + math.Pow(p.y,2)
+	return math.Sqrt(t)
+}
+
+func main(){
+	p1 := Point{3,4}
+	fmt.Println(p1)
+	fmt.Println(p1.DistToOrigin())
+	fmt.Println(p1)
+}
+
+// With Pointer Reference, podemos contornar o problema das limitações dos métodos
+
+// 1. Muito tempo para copiar a CHAMA POR VALOR quando temos valores a passar muito grande para os métodos;
+// 2. Métodos não podem transformar dados dentro do receiver por se tratarem de cópias
+// Podemos passar os ponteiros em uma CHAMADA POR REFERÊNCIA que estas limitações dos métodos são contornadas.
