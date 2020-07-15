576b0cd837d06ef196f6f16ddbcd46c7515144b8
Lucas Lima <64433440+LucasLimaPinho@users.noreply.github.com>
2020-07-14 21:14:07 -0300

    Update methodwithPointerReference.go

--- methodwithPointerReference.go
+++ methodwithPointerReference.go
@@ -27,3 +27,10 @@ func main(){
 // 1. Muito tempo para copiar a CHAMA POR VALOR quando temos valores a passar muito grande para os métodos;
 // 2. Métodos não podem transformar dados dentro do receiver por se tratarem de cópias
 // Podemos passar os ponteiros em uma CHAMADA POR REFERÊNCIA que estas limitações dos métodos são contornadas.
+
+// Dereferencing is automatic with dot (.) operator 
+// DO not need to reference when calling the method 
+// Good Programming Practice
+//////////1. All methods for a type have pointer receivers;
+//////////2. All methods for a type have non-pointer receivers;
+///////// Mixing pointer/non-pointer receivers will get confusing - POinter receivers allows modifications.
